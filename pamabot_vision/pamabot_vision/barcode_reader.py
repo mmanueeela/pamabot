cc7510d40fbe75b013707ef86fa2e135c423f47e
Manuela Zazzaro <manussupv@gmail.com>
2025-05-14 10:18:18 +0200

    Cambios Manu |
    Arreglos finales

--- pamabot_vision/pamabot_vision/barcode_reader.py
+++ pamabot_vision/pamabot_vision/barcode_reader.py
@@ -14,7 +14,6 @@ class BarcodeReader(Node):
         self.bridge = CvBridge()
         self.subscription = self.create_subscription(Image, '/camera/image_raw', self.image_callback, 10)
         self.publisher = self.create_publisher(String, '/codigo_detectado', 10)
-        self.detected_codes = set()
         self.get_logger().info("🟢 Lector de códigos iniciado y esperando imágenes...")
 
     def image_callback(self, msg):
@@ -26,28 +25,25 @@ class BarcodeReader(Node):
 
         barcodes = decode(frame)
 
-        found_new_code = False
+        found_code = False
 
         for barcode in barcodes:
             content = barcode.data.decode('utf-8')
-            if content not in self.detected_codes:
-                self.detected_codes.add(content)
-                self.get_logger().info(f"📋 Código detectado: {content}")
+            self.get_logger().info(f"📋 Código detectado: {content}")
+            
+            msg_out = String()
+            msg_out.data = content
+            self.publisher.publish(msg_out)
+            self.get_logger().info("✅ Mensaje publicado en /codigo_detectado")
 
-                msg_out = String()
-                msg_out.data = content
-                self.publisher.publish(msg_out)
-                self.get_logger().info("✅ Mensaje publicado en /codigo_detectado")
+            # Dibujar rectángulo
+            pts = [(p.x, p.y) for p in barcode.polygon]
+            cv2.polylines(frame, [np.array(pts, np.int32)], True, (0, 255, 0), 2)
+            found_code = True
 
-                # Dibujar rectángulo
-                pts = [(p.x, p.y) for p in barcode.polygon]
-                cv2.polylines(frame, [np.array(pts, np.int32)], True, (0, 255, 0), 2)
-                found_new_code = True
-
-        # Mostrar solo si hubo código nuevo
-        if found_new_code:
+        if found_code:
             cv2.imshow("Lector de código", frame)
-            cv2.waitKey(3000)  # Muestra la imagen 3 segundos
+            cv2.waitKey(3000)
             cv2.destroyAllWindows()
 
 
@@ -61,4 +57,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
